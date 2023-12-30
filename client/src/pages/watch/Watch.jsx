8688f6be184bc93ce115c771d130676d4f55c1a7
Shubrotto Kumar Hawlader <skshubroto@gmail.com>
2023-12-31 01:57:34 +0600

    click anyone item go to watch page

--- client/src/pages/watch/Watch.jsx
+++ client/src/pages/watch/Watch.jsx
@@ -1,7 +1,10 @@
 import { ArrowBackOutlined } from "@mui/icons-material";
 import "./watch.scss";
+import { useLocation } from "react-router-dom";
 
 const Watch = () => {
+  const location = useLocation();
+  console.log(location);
   return (
     <div className="watch">
       <div className="back">
@@ -12,7 +15,6 @@ const Watch = () => {
         className="video"
         src="https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761"
         autoPlay
-        progress
         controls
       ></video>
     </div>
